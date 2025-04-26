7f4a545d32940e9130eab2a777e6c46bca11a7bb
gourishchouhan <gourishchouhan338@gmail.com>
2025-04-26 23:43:47 +0530

    Updated frontend and added shadcn/ui

---
+++ client/src/app/login/page.jsx
@@ -0,0 +1,14 @@
+// src/app/login/page.jsx
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {/* Login form will go here */}
+      <p>Form placeholder</p>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ client/src/app/register/page.jsx
@@ -0,0 +1,14 @@
+// src/app/register/page.jsx
+import React from "react";
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <h1>Register Page</h1>
+      {/* Registration form will go here */}
+      <p>Form placeholder</p>
+    </div>
+  );
+};
+
+export default RegisterPage;
