50b17246c303b40daad04c7d5bc4ca95a33e1c2a
gourishchouhan <gourishchouhan338@gmail.com>
2025-04-28 10:23:14 +0530

    Integrated Socket.IO

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -12,7 +14,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  methods: ["GET", "POST"],
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -23,8 +30,43 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/messages", messageRoutes);
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  pingTimeout: 60000,
+  cors: corsOptions
+});
+
+io.on("connection", (socket) => {
+  console.log(`Socket connected: ${socket.id}`);
+
+  socket.on("setup", (userData) => {
+      console.log(`Setup event received for user: ${userData?._id} on socket ${socket.id}`);
+      socket.join(userData._id);
+      console.log(`Socket ${socket.id} joined room: ${userData._id}`);
+      socket.emit("connected");
+  });
+
+  socket.on("sendMessage", (messageData) => {
+      console.log("Message received on backend:", messageData);
+      const receiverRoom = messageData.receiver;
+      if (receiverRoom) {
+          console.log(`Emitting message to room: ${receiverRoom}`);
+          socket.to(receiverRoom).emit("receiveMessage", messageData);
+      }
+  });
+
+  socket.on("typing", (room) => socket.to(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.to(room).emit("stop typing"));
+
+  socket.on("disconnect", () => {
+    console.log(`Socket disconnected: ${socket.id}`);
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server (including Socket.IO) is running on port ${PORT}`);
 });
+
